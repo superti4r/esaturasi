dbd2cc1526c0bbe74162f4a4add31b2a009062dd
Bachtiar Dwi Pramudi <142956484+superti4r@users.noreply.github.com>
2025-02-28 16:03:29 +0700

    Adjusting Jadwal on Guru with Arsip (Jadwal show when Arsip "Aktif") 👍

--- app/Http/Controllers/Guru/DataJadwal.php
+++ app/Http/Controllers/Guru/DataJadwal.php
@@ -8,14 +8,16 @@ use App\Models\PembagianJadwal;
 
 class DataJadwal extends Controller
 {
-
     function index(){
         $user = auth()->user();
         $jadwal = PembagianJadwal::where('guru_id', $user->id)
-        ->with(['kelas', 'jadwalMataPelajaran'])
-        ->orderBy('hari')
-        ->orderBy('jam_mulai')
-        ->get();
+            ->whereHas('arsip', function ($query) {
+                $query->where('status', 'aktif');
+            })
+            ->with(['kelas', 'jadwalMataPelajaran', 'arsip'])
+            ->orderBy('hari')
+            ->orderBy('jam_mulai')
+            ->get();
 
         return view('guru.jadwal.index', compact('jadwal'));
     }
