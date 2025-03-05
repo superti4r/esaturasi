3b58e9608e6138d3bd64abdc9e94afc5cf26b4da
Bachtiar Dwi Pramudi <142956484+superti4r@users.noreply.github.com>
2025-03-05 16:32:57 +0700

    Migration Slug + Tugas + Materi 👍

---
+++ database/migrations/2025_03_04_155053_create_tugas_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tugas', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('slug_id')->constrained('slugs')->onDelete('cascade');
+            $table->string('judul')->nullable();
+            $table->text('deskripsi')->nullable();
+            $table->string('file')->nullable();
+            $table->dateTime('deadline')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tugas');
+    }
+};
