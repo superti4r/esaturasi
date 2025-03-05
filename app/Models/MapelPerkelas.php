ec5562cd6bbef579a95504cddbeb091be6f76756
Bachtiar Dwi Pramudi <bchtrrprmd@gmail.com>
2025-03-06 04:44:44 +0700

    Renaming Typo 👍

--- app/Models/MapelPerkelas.php
+++ app/Models/MapelPerkelas.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class MapelPerKelas extends Model
+class MapelPerkelas extends Model
 {
     use HasFactory;
 
