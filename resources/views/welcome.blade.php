f6e10624bf0a26e6b185c83ad0cd628c4ba2cb4e
Bachtiar Dwi Pramudi <142956484+superti4r@users.noreply.github.com>
2025-03-18 22:36:15 +0700

    Fix img src 👍

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -301,7 +301,7 @@
   <a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
   <div id="preloader"></div>
 
-  <script src="{{ asset('home/vendor/bootstrap/js/bootstrap.bundle.min.js')}}assets/"></script>
+  <script src="{{ asset('home/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
   <script src="{{ asset('home/vendor/php-email-form/validate.js')}}"></script>
   <script src="{{ asset('home/vendor/aos/aos.js')}}"></script>
   <script src="{{ asset('home/vendor/glightbox/js/glightbox.min.js')}}"></script>
