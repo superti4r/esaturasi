e89be73bcaf1b99870e4a0f7920966ea8acfbf88
Bachtiar Dwi Pramudi <142956484+superti4r@users.noreply.github.com>
2025-03-22 16:18:52 +0700

    Bootstraping 👍

--- routes/web.php
+++ routes/web.php
@@ -22,16 +22,6 @@ use App\Http\Controllers\Guru\DataTugasDanMateriController;
 use App\Http\Controllers\Guru\DataUjian;
 use FontLib\Table\Type\name;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::middleware(['guest'])->group(function () {
     Route::get('/', function () {return view('welcome');});
