83e5851b0c6977cbc02b96b195654909c3bba6b7
Bachtiar Dwi Pramudi <bchtrrprmd@gmail.com>
2026-05-18 21:58:48 +0700

    Enhance MySQL and PHP configurations: improve user creation script, update Prometheus targets, and add production PHP settings

--- docker/mysql/init/01-create-exporter.sh
+++ docker/mysql/init/01-create-exporter.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
+# This script is executed when the MySQL container is first started.
+# It creates the dedicated user for the Prometheus MySQL exporter.
+
 set -e
 
-mysql -uroot -p"$MYSQL_ROOT_PASSWORD" <<SQL
-CREATE USER IF NOT EXISTS '${MYSQL_EXPORTER_USER}'@'%' IDENTIFIED BY '${MYSQL_EXPORTER_PASSWORD}';
-GRANT PROCESS, REPLICATION CLIENT, SELECT ON *.* TO '${MYSQL_EXPORTER_USER}'@'%';
-FLUSH PRIVILEGES;
-SQL
+# The script receives the root password via the MYSQL_ROOT_PASSWORD environment variable.
+# It's crucial that this script is idempotent. The `IF NOT EXISTS` clause ensures that.
+mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" <<-EOSQL
+    CREATE USER IF NOT EXISTS '${MYSQL_EXPORTER_USER}'@'%' IDENTIFIED WITH 'mysql_native_password' BY '${MYSQL_EXPORTER_PASSWORD}';
+    GRANT PROCESS, REPLICATION CLIENT, SELECT ON *.* TO '${MYSQL_EXPORTER_USER}'@'%';
+    FLUSH PRIVILEGES;
+EOSQL
